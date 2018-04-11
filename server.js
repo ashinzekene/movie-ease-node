1f1837e6f6b34146c66336cd252025c0c1467340
Ashinzekene <ashinzekene@gmail.com>
2018-04-11 01:52:34 +0100

    fixed port issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const seriesRoute = require('./routes/new-series')
 const imagesRoute = require('./routes/images-route')
 const botRoute = require('./routes/bot-route')
 const appendResponseRoute = require('./routes/append-response-route')
-const port = process.env.PORT || 5400
+const port = 5400
 
 app.use(cors({ origin: "https://ashinzekene.github.io" }))
 app.use(bodyParser.urlencoded({extended: false}))
