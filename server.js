615db90585d89a5da8d690a059de21826977e9fb
Ashinzekene <ashinzekene@gmail.com>
2018-06-15 15:22:25 +0100

    CORS

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-const express = require('express')
-const path = require("path")
-const http = require("http")
-const bodyParser = require("body-parser")
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const moviesRoute = require('./routes/new-movies')
-const actorsRoute = require('./routes/new-actors')
-const seriesRoute = require('./routes/new-series')
-const imagesRoute = require('./routes/images-route')
-const botRoute = require('./routes/bot-route')
-const appendResponseRoute = require('./routes/append-response-route')
-const port = process.env.PORT || 5400
+const moviesRoute = require('./routes/new-movies');
+const actorsRoute = require('./routes/new-actors');
+const seriesRoute = require('./routes/new-series');
+const imagesRoute = require('./routes/images-route');
+const botRoute = require('./routes/bot-route');
+const appendResponseRoute = require('./routes/append-response-route');
+const port = process.env.PORT || 5400;
 
 // app.use(cors({ origin: ["https://http://ashinzekene.github.io/movie-ease-bot"], credentials: ["https://http://ashinzekene.github.io/movie-ease-bot"] }))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", [ "https://http://ashinzekene.github.io/movie-ease-bot"])
-  res.setHeader("Access-Control-Allow-Credentials", "true")
-  next()
-})
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.use('/movies', moviesRoute)
-app.use('/actors', actorsRoute)
-app.use('/series', seriesRoute)
-app.use('/bot', botRoute)
-app.use('/a2r', appendResponseRoute)
-app.use('/images', imagesRoute)
+app.use('/movies', moviesRoute);
+app.use('/actors', actorsRoute);
+app.use('/series', seriesRoute);
+app.use('/bot', botRoute);
+app.use('/a2r', appendResponseRoute);
+app.use('/images', imagesRoute);
 
 module.exports = app.listen(port, function(err) {
-  if(err) console.log(err)
-  console.log('It is happening at port ', port)
-})
+  if (err) console.log(err);
+  console.log('It is happening at port ', port);
+});
 
-require("./routes/bot")
+require('./routes/bot');
