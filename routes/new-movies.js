253dedf76d89f3dcc0fb071c325e38b354e95e96
Ashinzekene <ashinzekene@gmail.com>
2018-06-15 15:28:57 +0100

    added short movie

--- routes/new-movies.js
+++ routes/new-movies.js
@@ -1,42 +1,40 @@
-var express = require("express")
-var movies = require("../functions/movies")
-var router = express.Router()
-
+var express = require('express');
+var movies = require('../functions/movies');
+var router = express.Router();
 
 router.get('*', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  console.log(req.originalUrl)
-  next()
-})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log(req.originalUrl);
+  next();
+});
 router.get('/', function(req, res, next) {
-  res.status(200).json({result: 'You requested for movies'})
-})
+  res.status(200).json({ result: 'You requested for movies' });
+});
 router.get('/one/:id', function(req, res, next) {
-  if (req.short.short === 'yes') {
-    movies.oneShort(req.params.id).then(result => res.status(200).json(result))
-    return
+  if (req.params.short === 'yes') {
+    movies.oneShort(req.params.id).then(result => res.status(200).json(result));
+    return;
   }
-  movies.one(req.params.id).then(result => res.status(200).json(result))
-})
+  movies.one(req.params.id).then(result => res.status(200).json(result));
+});
 router.get('/popular/:page', function(req, res) {
-  movies.popular(req.params.page).then(result => res.status(200).json(result))
-})
+  movies.popular(req.params.page).then(result => res.status(200).json(result));
+});
 router.get('/top-rated/:page', function(req, res) {
-  movies.top(req.params.page).then(result => res.status(200).json(result))
-})
+  movies.top(req.params.page).then(result => res.status(200).json(result));
+});
 router.get('/latest/:page', function(req, res) {
-  movies.latest(req.params.page).then(result => res.status(200).json(result))
-})
+  movies.latest(req.params.page).then(result => res.status(200).json(result));
+});
 router.get('/upcoming/:page', function(req, res) {
-  movies.upcoming(req.params.page).then(result => res.status(200).json(result))
-})
+  movies.upcoming(req.params.page).then(result => res.status(200).json(result));
+});
 router.get('/search/:query', function(req, res) {
-  movies.search(req.params.query).then(result => res.status(200).json(result))
-})
-
+  movies.search(req.params.query).then(result => res.status(200).json(result));
+});
 
 router.get('*', function(req, res) {
-  res.status(400).json({response : 'Bad url'})
-})
+  res.status(400).json({ response: 'Bad url' });
+});
 
-module.exports = router
+module.exports = router;
